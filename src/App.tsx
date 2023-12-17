2661a695c7ac210ec827071467ce7b6851c6b0bd
preetipanwar <preetipanwar102@gmail.com>
2023-12-17 22:39:14 +0530

    listed movie poster

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import Square1 from './components/Square1';
+import { movies } from './movies';
 
 function App() {
   return (
-    <>
-      <Square1 />
-    </>
+    <div className="flex justify-center">
+      <div className="grid grid-cols-6 gap-6 m-6">
+        {movies.map((movie) => (
+          <div className="flex flex-col justify-between p-3 w-48 h-90 rounded-xl backdrop-blur-sm bg-black/30 ">
+            <img
+              className="rounded-lg"
+              src={'https://image.tmdb.org/t/p/original' + movie.poster_path}
+            />
+            <p className="mt-2 text-center font-semibold text-sm py-2">
+              {movie.title}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
