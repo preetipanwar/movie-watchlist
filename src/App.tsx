ee6ed11cd7052ae2897623885aa4f1d60f318681
preetipanwar <preetipanwar102@gmail.com>
2023-12-18 19:59:41 +0530

    finished all the functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import { useState } from 'react';
 import { movies } from './movies';
+import Movie from './components/Movie';
 
-function App() {
+export default function App() {
   const [watchList, setWatchList] = useState<any>([]);
+
+  const handleClick = (movie) => {
+    const clone = [...watchList];
+
+    // index, -1
+    const index = clone.findIndex((item) => {
+      return item.id === movie.id;
+    });
+
+    if (index === -1) {
+      // item is not in array
+      setWatchList([...watchList, movie]);
+    } else {
+      // item
+      clone.splice(index, 1);
+      setWatchList(clone);
+    }
+  };
 
   return (
     <div className="flex flex-col justify-center">
       <header className="flex h-28 px-12 z-10 items-center justify-between backdrop-blur-sm bg-black/50 w-full fixed top-0 text-2xl font-semibold text-gray-300 text-left">
-        <p className="text-pink-500 italic">Watched movies</p>
+        <p className="text-pink-500 italic">Watched Movies</p>
         <p className="p-2 text-pink-300 bg-pink-900 rounded-lg">
           {watchList.length}
           <span className="text-pink-200 font-medium"> / </span>
@@ -16,38 +35,13 @@ function App() {
       </header>
       <div className="grid grid-cols-8 gap-6 m-6 mt-32">
         {movies.map((movie) => (
-          <div
-            onClick={() => {
-              const clone = [...watchList];
-
-              const result = clone.find((item) => {
-                return item.id === movie.id;
-              });
-
-              // if (result) {
-              //   // do nothing
-              // } else {
-              //   setWatchList([...watchList, movie]);
-              // }
-
-              if (!result) {
-                setWatchList([...watchList, movie]);
-              }
-            }}
-            className="flex flex-col justify-between p-3 w-42 h-78 rounded-xl backdrop-blur-sm bg-black/30 opacity-40 hover:opacity-100 hover:scale-110 transition ease-in-out delay-75"
-          >
-            <img
-              className="rounded-lg"
-              src={'https://image.tmdb.org/t/p/original' + movie.poster_path}
-            />
-            <p className="mt-2 text-center font-semibold text-sm py-2">
-              {movie.title}
-            </p>
-          </div>
+          <Movie
+            movie={movie}
+            handleClick={handleClick}
+            watchList={watchList}
+          />
         ))}
       </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Movie.tsx
@@ -0,0 +1,25 @@
+export default function Movie({ movie, handleClick, watchList }) {
+  const clone = [...watchList];
+
+  const result = clone.find((item) => {
+    return item.id === movie.id;
+  });
+
+  return (
+    <div
+      className="flex flex-col justify-between p-3 w-42 h-78 rounded-xl backdrop-blur-sm bg-black/30 opacity-40 hover:opacity-100 hover:scale-110 transition ease-in-out delay-75"
+      style={
+        result ? { borderWidth: 4, borderColor: 'brown', opacity: 100 } : {}
+      }
+      onClick={() => handleClick(movie)}
+    >
+      <img
+        className="rounded-lg"
+        src={'https://image.tmdb.org/t/p/original' + movie.poster_path}
+      />
+      <p className="mt-2 text-center font-semibold text-sm py-2">
+        {movie.title}
+      </p>
+    </div>
+  );
+}
